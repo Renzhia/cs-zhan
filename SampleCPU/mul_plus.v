d6d91755873ecdcdac67cf6190c14d21f4b6234a
Renzhia <moralesdjerom438@gmail.com>
2024-12-27 22:01:24 +0800

    自制乘法器实现

--- SampleCPU/mul_plus.v
+++ SampleCPU/mul_plus.v
@@ -41,6 +41,51 @@ module mul_plus(
     assign op1_absolute = op1_sign ? (~opdata1_i+1) : opdata1_i;
     assign op2_absolute = op2_sign ? (~opdata2_i+1) : opdata2_i;
 
+    reg [63:0] multiplicand;  // 被乘数寄存器
 
+    // 被乘数左移控制
+    always @ (posedge clk) begin
+        if (judge) begin              // 运算过程中，被乘数每次左移一位
+            multiplicand <= {multiplicand[62:0],1'b0};
+        end
+        else if (start_i) begin       // 开始时，加载操作数
+            multiplicand <= {32'd0,op1_absolute};
+        end
+    end
+
+    // 乘数右移控制
+    always @ (posedge clk) begin
+        if(judge) begin               // 运算过程中，乘数每次右移一位
+            multiplier <= {1'b0,multiplier[31:1]};
+        end
+        else if(start_i) begin        // 开始时，加载操作数
+            multiplier <= op2_absolute;
+        end
+    end
+
+    // 根据乘数最低位决定是否需要加上部分积
+    assign temporary_value = multiplier[0] ? multiplicand : 64'd0;
+
+    // 累加部分积
+    always @ (posedge clk) begin
+        if (judge) begin              // 运算过程中，累加部分积
+            mul_temporary <= mul_temporary + temporary_value;
+        end
+        else if (start_i) begin       // 开始时，清零累积结果
+            mul_temporary <= 64'd0;
+        end
+    end
+
+    // 确定最终结果的符号
+    always @ (posedge clk) begin
+        if (judge) begin              // 运算过程中，根据输入操作数的符号确定结果符号
+            result_sign <= op1_sign ^ op2_sign;
+        end
+    end
+
+    // 根据符号位决定是否需要对结果取补
+    assign result_o = result_sign ? (~mul_temporary+1) : mul_temporary;
+    // 当乘数为0时，表示运算完成
+    assign ready_o = judge & multiplier == 32'b0;
 
 endmodule
